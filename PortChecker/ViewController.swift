f5f0130fd44e4e449e31fdf0117b980ee632a488
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-03-17 15:51:29 +0300

    Initial Commit

---
+++ PortChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PortChecker
+//
+//  Created by Alexey Tsymlov on 3/17/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
